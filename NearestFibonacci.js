22d349658f8f7a8e0da80b785fe5921b43c1d1cb
Seen3 <63313585+Seen3@users.noreply.github.com>
2020-08-21 12:43:59 +0530

    Update NearestFibonacci.js

--- NearestFibonacci.js
+++ NearestFibonacci.js
@@ -1,1 +1,35 @@
 //this function gives the nearest fibonacci number to the number given
+function nearest(s,fir,sec)
+{
+    if(Math.abs(sec-s)<=Math.abs(fir-s)){
+        return sec;
+    }
+    return fir;
+}
+function nearestFib(searchNum,first=1,second=1)
+{
+    if (searchNum>=first && searchNum<=second)
+    {
+        return nearest(searchNum,first,second);
+    }
+    let temp=first;
+    first=second;
+    second=first+temp;
+    return nearestFib(searchNum,first,second);
+}
+function test()
+{
+  if (nearestFib(54)!=55)
+  {
+    return false;
+  }
+  if (nearestFib(99)!=89)
+  {
+    return false;
+  }
+  if(nearestFib(11)!=13)
+  {
+    return false;
+  }
+  return true;
+}
